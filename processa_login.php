937e312e16aaa3cfd5e6b823b0dc7388ccda500a
ISABELA VICTORIA DE NOVAIS ROMANATO <21193062@CFP127ALUNO.LOCAL>
2021-09-23 10:59:55 -0300

    processa login

--- processa_login.php
+++ processa_login.php
@@ -1,5 +1,29 @@
 <?php
 
+//INICIALIZA A SESSÃO
+session_start();
+
+//IMPORTAÇÃO DO ARQUIVO DE FUNCOES
+
+require_once ('./funcoes.php');
+
+//RECEBENDO OS DADOS DO FORMULÁRIO
+
+if(isset($_POST["txt_usuario"]) || isset($_POST["txt_senha"])){
+
+    $usuario = $_POST["txt_usuario"];
+    $senha = $_POST["txt_senha"];
+
+// var_dump($_POST);exit;
+
+realizarLogin($usuario, $senha, lerArquivo("./dados/usuarios.json"));
+
+} else if ($_GET["logout"]) {
+
+    finalizarLogin();
+}
+
+
 
 
 ?>
